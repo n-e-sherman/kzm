81641fe09e3378674e3513c8650dbddea1b9b311
Nick Sherman <n.e.sherman@berkeley.edu>
2021-09-29 11:35:35 -0700

    added multiprocessing

--- driver.py
+++ driver.py
@@ -1,61 +1,87 @@
 from kzm import KZM
+import sys
 import os
+import subprocess
+import multiprocessing
 import numpy as np
 
+cores = 1
+nodes = 1
+node = 0
+if len(sys.argv) > 1:
+    cores = int(sys.argv[1])
+if len(sys.argv) > 2:
+    nodes = int(sys.argv[2])
+if len(sys.argv) > 3:
+    node = int(sys.argv[3])
+print(cores, nodes, node)
+
+def run(options):
+    kzm = KZM(options)
+    kzm.evolve()
 
 
-vi = 1E-5
-vf = 0.5
-Nv = 50
-x = np.log(vf/vi)/(Nv-1)
-vs = [vi*np.exp(n*x) for n in range(Nv)][::-1]
-chis = [2*(1+x) for x in range(10)]
-
-# testing
-# vs = [1]
-# chis = [2]
 
 
-options = {
-    'ramp' : 'linear', # [linear, smooth]
-    'path' : 'g', # [both, g]
-    'endpoint' : 'critical', # [critical, ising]
-    'gi' : 3,
-    'v' : 0.1,
-    'dt' : 0.1,
-    'chi': 4,
-    'evolver': 'TDVP',
-    'repo': 'pickle',
-    'load': True
-}
 
-tdvp_options = {
-    
-}
 
-tebd_options = {
-    'order': 2,
-}
 
-dmrg_options = {
-    
-}
 
-repo_options = {
-    'cwd' : os.getcwd(),
-    'data_dir' : '.data/',
-    'res_dir' : '.results/',
-    'log_dir' : '.log/'
-}
 
-options['tdvp_options'] = tdvp_options
-options['tebd_options'] = tebd_options
-options['dmrg_options'] = dmrg_options
-options['repo_options'] = repo_options
 
-for v in vs:
-    for chi in chis:
-        options['chi'] = chi
-        options['v'] = v
-        kzm = KZM(options)
-        kzm.evolve()
+
+if __name__ == '__main__':
+
+    options = {
+        'ramp' : 'linear', # [linear, smooth]
+        'path' : 'g', # [both, g]
+        'endpoint' : 'critical', # [critical, ising]
+        'gi' : 3,
+        'v' : 0.1,
+        'dt' : 0.1,
+        'chi': 4,
+        'evolver': 'TDVP',
+        'repo': 'pickle',
+        'load': True
+    }
+
+    tdvp_options = {
+        
+    }
+
+    tebd_options = {
+        'order': 2,
+    }
+
+    dmrg_options = {
+        
+    }
+
+    repo_options = {
+        'cwd' : os.getcwd(),
+        'data_dir' : '.data/',
+        'res_dir' : '.results/',
+        'log_dir' : '.log/'
+    }
+
+    options['tdvp_options'] = tdvp_options
+    options['tebd_options'] = tebd_options
+    options['dmrg_options'] = dmrg_options
+    options['repo_options'] = repo_options
+
+    vi = 1E-5
+    vf = 0.5
+    Nv = 50
+    x = np.log(vf/vi)/(Nv-1)
+    vs = [vi*np.exp(n*x) for n in range(Nv)][::-1]
+    chis = [2*(1+x) for x in range(10)]
+
+    args = []
+    for v in vs:
+        for chi in chis:
+            arg = options.copy()
+            arg['chi'] = chi
+            arg['v'] = v
+            args.append(arg)
+    p = multiprocessing.Pool(cores)
+    p.map(run, args[node::nodes])
